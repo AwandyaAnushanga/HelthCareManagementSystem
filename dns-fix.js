826817e9a95963579fd025c8ad4ee64ecc9fed20
Awandya <it22356154@my.sliit.lk>
2026-04-17 22:36:26 +0530

    final touch

--- dns-fix.js
+++ dns-fix.js
@@ -1,6 +1,3 @@
-// Preload script: fixes DNS resolution for MongoDB Atlas on Windows
-// The OS DNS resolver (dns.lookup) fails, but dns.resolve4 with Google DNS works.
-// This overrides dns.lookup to use dns.resolve4 as a fallback.
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
@@ -10,12 +7,10 @@ dns.lookup = function (hostname, options, callback) {
     callback = options;
     options = {};
   }
-  // Try original lookup first, fallback to resolve4 on failure
   originalLookup.call(dns, hostname, options, (err, address, family) => {
     if (!err) return callback(null, address, family);
-    // Fallback to dns.resolve4
     dns.resolve4(hostname, (err2, addresses) => {
-      if (err2) return callback(err); // return original error
+      if (err2) return callback(err);
       callback(null, addresses[0], 4);
     });
   });
